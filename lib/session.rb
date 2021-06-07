646844762bc446d0d0d7a85fb5129fdf55c707a2
wliu <wliu0709@gmail.com>
2021-06-07 17:32:11 -0400

    updating session

--- lib/session.rb
+++ lib/session.rb
@@ -4,12 +4,16 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    
+
   end
 
   def [](key)
+      
   end
 
   def []=(key, val)
+    
   end
 
   # serialize the hash into json and save in a cookie
